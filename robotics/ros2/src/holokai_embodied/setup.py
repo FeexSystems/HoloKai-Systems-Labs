b730152853ae93e5cc351741aa71393077b70a31
FeexSystems Labs <105814969+FeexSystems@users.noreply.github.com>
2026-08-17 11:15:23 -0500

    feat(perception): register Isaac artifact adapter

--- robotics/ros2/src/holokai_embodied/setup.py
+++ robotics/ros2/src/holokai_embodied/setup.py
@@ -4,7 +4,7 @@ package_name = 'holokai_embodied'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.3.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
@@ -27,7 +27,9 @@ setup(
             'safety_gateway = holokai_embodied.safety_gateway:main',
             'world_model_bridge = holokai_embodied.world_model_bridge:main',
             'world_model = holokai_embodied.world_model:main',
+            'semantic_perception_bridge = holokai_embodied.semantic_perception_bridge:main',
             'isaac_ros_guard = holokai_embodied.isaac_ros_guard:main',
+            'isaac_ros_artifact_adapter = holokai_embodied.isaac_ros_artifact_adapter:main',
         ],
     },
 )
